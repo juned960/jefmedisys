49cdfcd139a831a8d116799c558c1c5fc0570ef4
Juned Aslam <juned@easyecom.io>
2023-02-18 23:12:14 +0530

    first commit

---
+++ app/Http/Controllers/jefmedisyscontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class jefmedisyscontroller extends Controller
+{
+    //
+    function home(){
+        return view('home');
+    }
+}
